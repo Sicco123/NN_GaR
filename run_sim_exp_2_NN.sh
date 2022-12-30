ea80ceb87d5aa7ce8f5ad680be72616e63f46a4f
siccokooiker <s.h.kooiker@student.vu.nl>
2022-12-30 17:11:37 +0100

    updated names to STAR

--- run_sim_exp_2_NN.sh
+++ run_sim_exp_2_NN.sh
@@ -3,10 +3,10 @@ eval "$(conda shell.bash hook)"
 conda activate base
 
 echo Experiment 150 is running...
-python3 sim_experiment_2_SESTAR.py configurations/nardl_150_221014.json
+python3 sim_experiment_2_STAR.py configurations/star_150_221014.json
 
 echo Experiment 250 is running...
-python3 sim_experiment_2_SESTAR.py configurations/nardl_250_221014.json
+python3 sim_experiment_2_STAR.py configurations/star_250_221014.json
 
 echo Experiment 500 is running...
-python3 sim_experiment_2_SESTAR.py configurations/nardl_500_221014.json
+python3 sim_experiment_2_STAR.py configurations/star_500_221014.json
